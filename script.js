764a3b7c35b019320fc17dc35d9c9d1ce58aa999
rauancamozzi <rauangabrielcamozzi@gmail.com>
2021-02-15 15:20:24 -0300

    :sparkles: Definir movimentos

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ snake[0] = {
     y: 8 * box
 };
 
+let direction = "right";
+
 function createBackground() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -20,5 +22,27 @@ function createSnake() {
     }
 }
 
-createBackground();
-createSnake();
+function startGame() {
+    createBackground();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX += box;
+    if(direction == "up") snakeY += box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
+
