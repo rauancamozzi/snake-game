4f50efa73a0fc4c3ed2bdca906d0eafbf347c1d0
rauancamozzi <rauangabrielcamozzi@gmail.com>
2021-02-15 16:15:47 -0300

    :sparkles: Estilizando a página

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ let food ={
 }
 
 function createBackground(){
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "#1dd1a1";
     context.fillRect(0, 0, 16*box, 16*box); 
 }
 
 function createSnake (){
     for(i = 0; i < snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = "#10ac84";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
 function drawFood (){
-    context.fillStyle = "red";
+    context.fillStyle = "#ff6b6b";
     context.fillRect(food.x, food.y, box, box);
 }
 
